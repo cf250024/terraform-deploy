843309d0ce0a9cd809a66b703ad597d4eab44e6a
cf250024 <chengjun.fu@teradata.com>
2021-04-27 18:50:22 -0700

    efs-csi-driver

--- aws-examples/blog-post/aws/efs.tf
+++ aws-examples/blog-post/aws/efs.tf
@@ -1,9 +1,89 @@
+# Create IAM role + automatically make it available to efs csi driver service account
+module "iam_assumable_role_admin_efs" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  create_role                   = true
+  role_name                     = "efs-csi-driver-jupyter"
+  provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.efs-csi-driver.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:efs-csi-controller-sa"]
+
+  tags = {
+    Owner = split("/", data.aws_caller_identity.current.arn)[1]
+    AutoTag_Creator = data.aws_caller_identity.current.arn
+  }
+}
+
+resource "aws_iam_policy" "efs-csi-driver" {
+  name_prefix = "cluster-efs"
+  description = "EKS efs-csi-driver policy for cluster ${module.eks.cluster_id}"
+  policy      = data.aws_iam_policy_document.efs-csi-driver.json
+}
+
+data "aws_iam_policy_document" "efs-csi-driver" {
+  statement {
+    sid       = "efs1"
+    effect    = "Allow"
+
+    actions   = [
+      "elasticfilesystem:DescribeAccessPoints",
+      "elasticfilesystem:DescribeFileSystems",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid        = "efs2"
+    effect     = "Allow"
+
+    actions    = [
+      "elasticfilesystem:CreateAccessPoint",
+    ]
+
+    resources  = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:RequestTag/efs.csi.aws.com/cluster"
+      values   = ["true"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:RequestTag/cluster-name"
+      values   = ["${var.cluster_name}"]
+    }
+  }
+
+  statement {
+    sid        = "efs3"
+    effect     = "Allow"
+
+    actions    = [
+      "elasticfilesystem:DeleteAccessPoint",
+    ]
+
+    resources  = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/efs.csi.aws.com/cluster"
+      values   = ["true"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:RequestTag/cluster-name"
+      values   = ["${var.cluster_name}"]
+    }
+  }
+}
+
 resource "aws_efs_file_system" "home_dirs" {
   tags = {
     Name = "${var.cluster_name}-home-dirs"
   }
 }
-
 
 resource "aws_security_group" "home_dirs_sg" {
   name   = "home_dirs_sg"
@@ -49,74 +129,15 @@ resource "helm_release" "efs-provisioner" {
     name = "storageClasses[0].parameters.fileSystemId"
     value = aws_efs_file_system.home_dirs.id
   }
-}
 
-resource "aws_iam_policy" "aws_efs_csi_driver" {
-  name   = "efs-csi-policy"
-  policy = <<-EOD
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "elasticfilesystem:DescribeAccessPoints",
-          "elasticfilesystem:DescribeFileSystems"
-        ],
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "elasticfilesystem:CreateAccessPoint"
-        ],
-        "Resource": "*",
-        "Condition": {
-          "StringLike": {
-            "aws:RequestTag/efs.csi.aws.com/cluster": "true",
-            "aws:RequestTag/cluster-name": "${var.cluster_name}"
-          }
-        }
-      },
-      {
-        "Effect": "Allow",
-        "Action": "elasticfilesystem:DeleteAccessPoint",
-        "Resource": "*",
-        "Condition": {
-          "StringEquals": {
-            "aws:ResourceTag/efs.csi.aws.com/cluster": "true",
-            "aws:ResourceTag/cluster-name": "${var.cluster_name}"
-          }
-        }
-      }
-    ]
+  set{
+    name  = "serviceAccount.controller.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_admin_efs.iam_role_arn
   }
-  EOD
-}
 
-resource "aws_iam_role" "efs_csi_role" {
-  name = "efs-csi-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::653480936020:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/0397DABF2ABB69344C70010FD2582753"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringEquals": {
-          "oidc.eks.us-west-2.amazonaws.com/id/0397DABF2ABB69344C70010FD2582753:sub": "system:serviceaccount:kube-system:efs-csi-controller-sa"
-        }
-      }
-    }
-  ]}
-EOF
-}
+   set{
+    name  = "controller.tags"
+    value = var.region
+  }
 
-resource "aws_iam_role_policy_attachment" "efs-csi-attach" {
-  role       = "${aws_iam_role.efs_csi_role.name}"
-  policy_arn = "${aws_iam_policy.aws_efs_csi_driver.arn}"
 }
